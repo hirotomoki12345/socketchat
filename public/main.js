3fa1a86fcc52876eee1d69bf473cc2f3556d039f
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-08-27 19:42:33 +0900

    Update main.js

--- public/main.js
+++ public/main.js
@@ -35,6 +35,9 @@ function checkTimeout() {
 checkTimeout();
 
 function escapeHtml(unsafe) {
+  if (typeof unsafe !== "string") {
+    return "";
+  }
   return unsafe
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
